2ad50721742887d0c1eb603890842ac56f526183
Ben Barber <barberboy@gmail.com>
2025-06-12 14:36:35 -0500

    Implement server-side searching, filtering, and pagination for advocates
    Additionally, this includes a schema change to model the specialties as an array of strings.

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,13 +1,34 @@
+import { arrayContains } from 'drizzle-orm';
+import { NextRequest } from "next/server";
+
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
 
-export async function GET() {
+/**
+ * GET /api/advocates
+ * 
+ * Returns advocates, optionally filtered by specialty.
+ * params:
+ *  - specialties: String | String[]
+ *  - limit: Number, default 10
+ *  - offset: Number, default 0
+ */
+export async function GET(request: NextRequest) {
+  // Support querying multiple specialties
+  const specialties = request.nextUrl.searchParams.getAll('specialty')
+  const limit = Number(request.nextUrl.searchParams.get('limit')) || 10
+  const offset = Number(request.nextUrl.searchParams.get('offset')) || 0
+
   const data = await db
     .select()
     .from(advocates)
-    // TODO: .where() filter by specialty 
-    .limit(50);
+    .where(
+      specialties.length
+        ? arrayContains(advocates.specialties, specialties)
+        : undefined
+    )
+    .limit(limit)
+    .offset(offset)
 
   return Response.json({ data });
 }
--- src/db/index.ts
+++ src/db/index.ts
@@ -2,18 +2,14 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 const setup = () => {
-  if (!process.env.DATABASE_URL) {
-    console.error("DATABASE_URL is not set");
-    return {
-      select: () => ({
-        from: () => [],
-      }),
-    };
-  } else {
-    const queryClient = postgres(process.env.DATABASE_URL);
+  const databaseURL = process.env.DATABASE_URL
+  if (databaseURL) {
+    const queryClient = postgres(databaseURL);
     const db = drizzle(queryClient);
-    return db;
-  };
+    return db
+  } else {
+    throw new Error("DATABASE_URL is not set in the environment")
+  }
 }
 
 export default setup();
