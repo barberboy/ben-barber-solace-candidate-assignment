423cf10b3ede113ae3914849fb3ca837dbe44835
Ben Barber <barberboy@gmail.com>
2025-06-20 09:34:28 -0500

    Advocates are sorted by experience or name
    Additionally, improved data fetching to use memoization via
    useCallback

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,4 +1,4 @@
-import { arrayContains } from 'drizzle-orm';
+import { arrayContains, asc, desc} from 'drizzle-orm';
 import { NextRequest } from "next/server";
 
 import db from "../../../db";
@@ -15,9 +15,16 @@ import { advocates } from "../../../db/schema";
  */
 export async function GET(request: NextRequest) {
   // Support querying multiple specialties
-  const specialties = request.nextUrl.searchParams.getAll('specialty')
-  const limit = Number(request.nextUrl.searchParams.get('limit')) || 10
-  const offset = Number(request.nextUrl.searchParams.get('offset')) || 0
+  const queryParams = request.nextUrl.searchParams;
+  const specialties = queryParams.getAll('specialty');
+  const limit = Number(queryParams.get('limit')) || 10;
+  const offset = Number(queryParams.get('offset')) || 0;
+  const sortBy = queryParams.get('sortBy') || 'lastName';
+
+  // Sort by either experience or last name/firstname
+  const orderBy = (sortBy === 'experience')
+    ? [desc(advocates.yearsOfExperience), asc(advocates.lastName), asc(advocates.firstName)]
+    : [asc(advocates.lastName), asc(advocates.firstName)];
 
   const data = await db
     .select()
@@ -29,6 +36,7 @@ export async function GET(request: NextRequest) {
     )
     .limit(limit)
     .offset(offset)
+    .orderBy(...orderBy)
 
   return Response.json({ data });
 }
