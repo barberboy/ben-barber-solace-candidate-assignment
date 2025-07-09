51e38a8fbbbf1f81c2eb356826909303b4d900ba
Ben Barber <barberboy@gmail.com>
2025-07-09 13:43:40 -0500

    Add specialties table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -9,16 +9,37 @@ import {
   index,
 } from "drizzle-orm/pg-core";
 
-export const advocates = pgTable("advocates", {
-  id: serial("id").primaryKey(),
-  firstName: text("first_name").notNull(),
-  lastName: text("last_name").notNull(),
-  city: text("city").notNull(),
-  degree: text("degree").notNull(),
-  specialties: text("specialties").array().notNull().default(sql`ARRAY[]::text[]`),
-  yearsOfExperience: integer("years_of_experience").notNull(),
-  phoneNumber: bigint("phone_number", { mode: "number" }).notNull(),
-  createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
-}, (table) => [
-  index('idx_specialties').using('gin', table.specialties)
-]);
+export const advocates = pgTable(
+  "advocates",
+  {
+    id: serial("id").primaryKey(),
+    firstName: text("first_name").notNull(),
+    lastName: text("last_name").notNull(),
+    city: text("city").notNull(),
+    degree: text("degree").notNull(),
+    specialties: text("specialties")
+      .array()
+      .notNull()
+      .default(sql`ARRAY[]::text[]`),
+    // specialties: text("specialties").array().notNull().default(sql`ARRAY[]::text[]`),
+    yearsOfExperience: integer("years_of_experience").notNull(),
+    phoneNumber: bigint("phone_number", { mode: "number" }).notNull(),
+    createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
+  },
+  (table) => [index("idx_specialties").using("gin", table.specialties)]
+);
+
+export const specialties = pgTable(
+  "specialties",
+  {
+    id: serial("id").primaryKey(),
+    name: text("name").notNull(),
+    color: text("color").notNull().default("#ddd"),
+  },
+  (table) => [
+    index("idx_specialty_name").using(
+      "gin",
+      sql`to_tsvector('english', ${table.name})`
+    ),
+  ]
+);
